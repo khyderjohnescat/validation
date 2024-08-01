a902955b1987395067c41404c61fa22044787b75
khyderjohnescat <escatkc.660.stud@cdd.edu.ph>
2024-08-01 14:42:46 +0800

    display all data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-        UserTask::factory(200)->create();
+        // UserTask::factory(200)->create();
 
         User::factory()->create([
             'name' => 'Test User',
